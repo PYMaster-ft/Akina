ba546a57448fa259b3fd5d81ad26a282280d1f1e
chamindu pushpika <72345334+PYMaster-ft@users.noreply.github.com>
2021-07-28 00:16:12 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,7 +102,7 @@ async function whatsAsena () {
                 }
             }
             else if (infoen !== '' && config.LANG == 'EN') {
-                while (getGMTh == 19 && getGMTm == 1) { 
+                while (getGMTh == 21 && getGMTm == 31) { 
                     return conn.sendMessage(conn.user.jid, '[ ```Daily Announcements``` ]\n\n' + infoen.replace('{user}', conn.user.name).replace('{wa_version}', conn.user.phone.wa_version).replace('{version}', config.VERSION).replace('{os_version}', conn.user.phone.os_version).replace('{device_model}', conn.user.phone.device_model).replace('{device_brand}', conn.user.phone.device_manufacturer), MessageType.text) 
                 }
             }
