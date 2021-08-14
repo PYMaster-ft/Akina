99630a19b89e817df574b687c6d26eb668d7b6cb
chamindu pushpika <72345334+PYMaster-ft@users.noreply.github.com>
2021-08-14 11:57:18 +0530

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -17,7 +17,7 @@ if (Config.WORKTYPE == 'private') {
 
         var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-        await message.client.sendMessage(message.jid, Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by JADUBot'})
+        await message.client.sendMessage(message.jid, Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '            ♔ ₳KIᑎᗩ ♔\n\n❍ Hosted ➤ AWS\n❍ Version ➤ V1.5 pro\n❍ Branch  ➤ master\n❍ About    ➤ https://tinyurl.com/mvdt8pnv\n❍ Owner   ➤https://wa.link/4kmqwf\n❍ Mail     ➤ systemzombiesx@gmail.com\n❍ Bot make video ➤ https://cutt.ly/7QH0x2\n\n⊱ ──────    ♔    ────── ⊰'})
 
     }));
 }
@@ -35,22 +35,7 @@ else if (Config.WORKTYPE == 'public') {
 
         var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-        await message.client.sendMessage(message.jid, Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by JADUBot'})
-
-    }));
-    Asena.addCommand({pattern: 'bot', fromMe: true, desc: 'bot link', dontAddCommandList: true}, (async (message, match) => {
-
-        var r_text = new Array ();
-
-        r_text[0] = "https://telegra.ph/file/220e93708af31d836b096.jpg";
-        r_text[1] = "https://telegra.ph/file/220e93708af31d836b096.jpg";
-        r_text[2] = "https://telegra.ph/file/220e93708af31d836b096.jpg";
-       
-        var i = Math.floor(3*Math.random())
-
-        var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
-
-        await message.client.sendMessage(message.jid, Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '     ♔ ₳KIᑎᗩ ♔\n\n❍ Hosted ➤ AWS\n❍ Version ➤ V1.5 pro\n❍ Branch  ➤ master\n❍ About    ➤ https://tinyurl.com/mvdt8pnv\n❍ Owner   ➤https://wa.link/4kmqwf\n❍ Mail     ➤ systemzombiesx@gmail.com\n❍ Bot make video ➤ https://cutt.ly/7QH0x2\n\n⊱ ──────    ♔    ────── ⊰'})
+        await message.client.sendMessage(message.jid, Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '            ♔ ₳KIᑎᗩ ♔\n\n❍ Hosted ➤ AWS\n❍ Version ➤ V1.5 pro\n❍ Branch  ➤ master\n❍ About    ➤ https://tinyurl.com/mvdt8pnv\n❍ Owner   ➤https://wa.link/4kmqwf\n❍ Mail     ➤ systemzombiesx@gmail.com\n❍ Bot make video ➤ https://cutt.ly/7QH0x2\n\n⊱ ──────    ♔    ────── ⊰'})
 
     }));
 }
