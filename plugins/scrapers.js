99c36f0f6c334d5bb133ecfe7a005fd5c77f946d
chamindu pushpika <72345334+PYMaster-ft@users.noreply.github.com>
2021-08-25 19:36:32 +0530

    Update scrapers.js
    👑 Youtube video list එකට නව මුහුණුවරක් එක් කර ලදී.

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -527,7 +527,7 @@ if (config.WORKTYPE == 'private') {
     
         var mesaj = '';
         arama.all.map((video) => {
-            mesaj += '*' + video.title + '* - ' + video.url + '\n'
+            mesaj += '🍻 *' + video.title + '* \n🔅 ' + video.url + '\n\n'
         });
 
         await message.client.sendMessage(message.jid,mesaj,MessageType.text);
@@ -996,7 +996,7 @@ else if (config.WORKTYPE == 'public') {
     
         var mesaj = '';
         arama.all.map((video) => {
-            mesaj += '*' + video.title + '* - ' + video.url + '\n'
+            mesaj += '🍻 *' + video.title + '* \n🔅 ' + video.url + '\n\n'
         });
 
         await message.client.sendMessage(message.jid,mesaj,MessageType.text);
